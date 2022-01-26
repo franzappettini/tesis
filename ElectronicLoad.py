f756a56f829f050148b9c49a787a22497444a99b
franzappettini <90067601+franzappettini@users.noreply.github.com>
2022-01-26 10:53:18 -0300

    Update ElectronicLoad.py

--- ElectronicLoad.py
+++ ElectronicLoad.py
@@ -4,31 +4,37 @@ import os
 import nidaqmx
 import csv
 import serial
+import configparser
+from configparser import ConfigParser
 
-class ElectronicLoad(object):
+class PowerSupply(object):
     def __init__(self, identity = ''):
         self.rm = pyvisa.ResourceManager()
         self.device_list = self.rm.list_resources()
-        print('Dispositivos conectados: '+ str(self.device_list))
         self.identity = identity
         if self.identity=='':
             self.identity = self.get_id()
-        self.electronic_load = self.rm.open_resource(self.identity)
+        self.power_supply = self.rm.open_resource(self.identity)
 
-    def voltage_min(self, voltage):
-        self.electronic_load.write(":SOUR:CURR:VON "+str(voltage))
+    def get_id(self):
+        for device_id in self.device_list:
+            if (len(device_id.split('::')) == 5 and
+                device_id.split('::')[3].startswith('DP')):
+                return device_id    
+        print ('Fuente de poder no detectada')
+        return ''
+            
+    def supply_voltage(self):
+        return float(self.power_supply.query(':MEAS:VOLT? CH3'))
 
-    def current_max(self, current):
-        self.electronic_load.write(":SOUR:CURR:LEV:IMM "+str(current))
-
-    def electronic_load_voltage(self):
-        return float(self.electronic_load.query(':MEAS:VOLT?'))
-
-    def electronic_load_current(self):
-        return float(self.query(':MEAS:CURR?'))   
+    def supply_current(self):
+        return float(self.power_supply.query(':MEAS:CURR? CH3'))
 
     def on(self):
-        self.electronic_load.write(':SOUR:INP:STAT ON')
+        self.power_supply.write(':OUTP CH3, ON')
 
     def off(self):
-        self.electronic_load.write(':SOUR:INP:STAT OFF')
+        self.power_supply.write(':OUTP CH3, OFF')
+
+    def voltage_current_max(self, voltage, current):
+        self.power_supply.write(":APPL CH3,"+str(voltage)+","+str(current))
